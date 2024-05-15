49224f45816414c2c2358d11e8eeaa6245662b37
Monkey_Elite <119488605+MonkeyElite@users.noreply.github.com>
2024-05-15 20:27:12 +0200

    Updated TODO S3, changed to using ViewModels in the API project

---
+++ ServerManager/ServerManagerApi/ServerManagerApi/Models/Request/RequestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ServerManagerApi.ViewModels.Request
+{
+    public class RequestViewModel(ServerManagerCore.Models.Request request)
+    {
+        public string Title { get; set; } = request.Title;
+        public string Description { get; set; } = request.Description;
+    }
+}
