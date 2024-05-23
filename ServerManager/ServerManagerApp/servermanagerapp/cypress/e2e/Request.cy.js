b17d3e2c25e7ad261846d793986075a1a5e695a2
Stijn <stijn2k4@gmail.com>
2024-05-23 16:05:42 +0200

    Added bad flow in endtoend create test

--- ServerManager/ServerManagerApp/servermanagerapp/cypress/e2e/Request.cy.js
+++ ServerManager/ServerManagerApp/servermanagerapp/cypress/e2e/Request.cy.js
@@ -1,4 +1,10 @@
-describe("template spec", () => {
+describe("Request Tests", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/request");
+    // Wait for the page to load fully
+    cy.contains("Create new request").should("be.visible");
+  });
+
   it("Creates Request Page", () => {
     cy.visit("http://localhost:3000/request");
 
@@ -27,6 +33,49 @@ describe("template spec", () => {
 
     // Verify that the navigation was successful (e.g., URL change)
     cy.url().should("include", "/request");
+  });
+
+  it("Validates Title and Description are Required", () => {
+    // Click the 'Create new request' button using data-testid
+    cy.get('[data-testid="create-request-button"]').click();
+
+    // Verify that the navigation was successful (e.g., URL change)
+    cy.url().should("include", "/request/create");
+
+    // Click the submit button without entering any data
+    cy.get('[data-testid="create-request-submit"]').click();
+
+    // Assert that error message for required fields is displayed
+    cy.contains(".text-red-600", "Title and description are required.").should(
+      "be.visible"
+    );
+
+    it("Validates Title Length", () => {
+      // Click the 'Create new request' button using data-testid
+      cy.get('[data-testid="create-request-button"]').click();
+
+      // Verify that the navigation was successful (e.g., URL change)
+      cy.url().should("include", "/request/create");
+
+      // Enter title longer than 255 characters
+      cy.get('[data-testid="create-request-title"]').type(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ".repeat(6) // 255 chars
+      );
+
+      // Enter valid description
+      cy.get('[data-testid="create-request-description"]').type(
+        "This is a valid description."
+      );
+
+      // Click the submit button
+      cy.get('[data-testid="create-request-submit"]').click();
+
+      // Assert that error message for title length is displayed
+      cy.contains(
+        ".text-red-600",
+        "Title must be less than or equal to 255 characters."
+      ).should("be.visible");
+    });
   });
 
   it("Updates Request", () => {
